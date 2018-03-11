2dbf61c89baeb0cbf57285ccf89bfb5714ff95e3
erm <jermff@gmail.com>
2018-03-11 22:32:45 +1100

    webapp

--- examples/webapp.py
+++ examples/webapp.py
@@ -6,8 +6,7 @@ from aioframe.loaders import load_apps_enabled
 from chat_project import conf
 
 
-async def run():
-    webapp = aiohttp.web.Application()
-    aiohttp_session.setup(webapp, conf.SESSION_STORAGE)
-    load_apps_enabled(webapp, conf)
-    aiohttp.web.run_app(webapp, host=conf.HOSTNAME, port=conf.PORT)
+webapp = aiohttp.web.Application()
+aiohttp_session.setup(webapp, conf.SESSION_STORAGE)
+load_apps_enabled(webapp, conf)
+# aiohttp.web.run_app(webapp, host=conf.HOSTNAME, port=conf.PORT)
