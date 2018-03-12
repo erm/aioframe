9cef9547ffa9b529843db688a997ad3e28ff2f26
erm <jermff@gmail.com>
2018-03-12 18:25:22 +1100

    Removing local, updating base

--- examples/chat_project/conf/base.py
+++ examples/chat_project/conf/base.py
@@ -12,24 +12,22 @@ from aioframe.models import DATABASE_PROXY
 APPS_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Session
+# Available options: simple_cookie, encrypted_cookie, redis
 
 SECRET_KEY = get_secret_key()
-
 SESSION_STORAGE = get_session_storage('encrypted_cookie', secret_key=SECRET_KEY)
 
 # Apps
 
 APPS_REGISTRY = (
     'chat',
-    'auth',
+    'auth'
 )
 
 # Server
 
 HOSTNAME = 'localhost'
-
 PORT = '8000'
-
 
 # Database
 
@@ -41,7 +39,6 @@ DATABASE = peewee_async.PooledPostgresqlDatabase(
 )
 DATABASE_PROXY.initialize(DATABASE)
 OBJECTS = peewee_async.Manager(DATABASE_PROXY)
-
 
 # Auth
 
