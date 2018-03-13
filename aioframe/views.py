c3e30ca649d0befc37787b8a655019054342484e
erm <jermff@gmail.com>
2018-03-14 01:15:17 +1100

    Starting on generic WebsocketBotView. Creating bots is going to be way easier now

--- aioframe/views.py
+++ aioframe/views.py
@@ -9,6 +9,13 @@ import jinja2
 
 class View(web.View):
 
+    """
+    Class-based view that subclasses aiohttp.web.View to allow registering routes to functions
+    within an aioframe app view.
+
+    Looks up handlers by app view class and request path.
+    """
+
     @property
     def view_class_name(self):
         return self.__class__.__qualname__
@@ -17,7 +24,6 @@ class View(web.View):
         request_path = self._request._rel_url.path
         match_info = self._request._match_info
         lookup_path = get_reverse_match(match_info) if match_info else request_path
-        print(lookup_path)
         app_conf = self._request.app['AIOFRAME_SETTINGS']['app_views'][self.view_class_name]['app_conf']
         try:
             view = app_conf.get_views(self.view_class_name)[lookup_path]
@@ -44,7 +50,8 @@ class TemplateView(View):
         super().__init__(*args, **kwargs)
         aiohttp_jinja2.setup(
             self._request.app,
-            loader=jinja2.FileSystemLoader(os.path.join(self._request.app['AIOFRAME_SETTINGS']['conf'].APPS_DIR, 'templates'))
+            loader=jinja2.FileSystemLoader(os.path.join(
+                self._request.app['AIOFRAME_SETTINGS']['conf'].APPS_DIR, 'templates'))
         )
 
 
@@ -52,22 +59,44 @@ class WebsocketView(View):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        self.ws_group_name = self._request._match_info['group_name']
         try:
             self._request.app['socket_groups']
         except KeyError:
-            self._request.app['socket_groups'] = {}
-
-    def get_ws_group(self, group_name):
-        try:
-            self._request.app['socket_groups'][group_name]
-        except KeyError:
-            self._request.app['socket_groups'][group_name] = {'name': group_name, 'sockets': [], 'data': {}, 'bot': None}
-        return self._request.app['socket_groups'][group_name]
+            self._request.app['socket_groups'] = {
+                self.ws_group_name: {
+                    'name': self.ws_group_name, 
+                    'sockets': [], 
+                    'data': {},
+                    'bot': None
+                }
+            }
+        self.ws_group = self._request.app['socket_groups'][self.ws_group_name]
 
     async def get_ws_response(self):
         response = web.WebSocketResponse()
         await response.prepare(self._request)
-        group_name = self._request._match_info['group_name']
-        ws_group = self.get_ws_group(group_name)
-        ws_group['sockets'].append(response)
-        return response, ws_group
+        self.ws_group['sockets'].append(response)
+        return response
+
+
+class WebsocketBot:
+
+    def __init__(self, ws_group):
+        self.ws_group = ws_group
+        self.data = {}
+
+    async def handle_msg(self, msg_data):
+        print(msg_data)
+        data = msg_data.split()
+        print(data)
+        for ws in self.ws_group['sockets']:
+            msg_resp = 'lol'
+            await ws.send_str(msg_resp)
+
+
+class WebsocketBotView(WebsocketView):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.ws_group['bot'] = WebsocketBot(self.ws_group)
--- examples/chat_project/chat/views.py
+++ examples/chat_project/chat/views.py
@@ -3,7 +3,7 @@ from aiohttp_jinja2 import render_template
 from aiohttp_session import get_session
 
 from aioframe.auth.utils import get_user_by_session, get_identity
-from aioframe.views import TemplateView, WebsocketView
+from aioframe.views import TemplateView, WebsocketView, WebsocketBotView
 from aioframe.clients import get_request
 from aioframe.apps.routes import get_url
 
@@ -14,76 +14,21 @@ from chat_project.conf import HOSTNAME, PORT, APPS_DIR
 from .app_conf import app
 
 
-class ChatBot:
-
-    def __init__(self, ws_group):
-        self.ws_group = ws_group
-        self.data = {}
-
-    async def handle_msg(self, msg_data):
-        # TODO: clean this up lol
-        response = {'msg': None}
-        data = msg_data[1:].split()
-        try:
-            command = data[0]
-        except IndexError:
-            response['msg'] = response['msg'] = 'Error: Not enough arguments.'
-        else:
-            args = data[1:]
-            if command == 'markovify':
-                try:
-                    src_url = args[0]
-                except IndexError:
-                    response['msg'] = 'Error: Not enough arguments.'
-                else:
-                    try:
-                        async with aiohttp.ClientSession(connector=aiohttp.TCPConnector(verify_ssl=False)) as session:
-                            text = await get_request(session, src_url)
-                            text_model = markovify.Text(text)
-                            self.data['markov'] = text_model
-                    except aiohttp.client_exceptions.InvalidURL:
-                        response['msg'] = 'Error: Invalid URL provided.'
-                    else:
-                        response['msg'] = 'Loaded data from {}'.format(src_url)
-            elif command == 'talk':
-                markov = self.data.get('markov')
-                if not markov:
-                    response['msg'] = 'Error: No data loaded, try !markovify <http://source.txt>'
-                else:
-                    sentences = [markov.make_sentence() for i in range(10)]
-                    sentences = list(set([s for s in sentences if s]))
-                    markov_msg = random.choice(sentences)
-                    response['msg'] = markov_msg
-            else:
-                response['msg'] = 'Error: valid commands are "markovify" and "talk"'
-        return response
-
 @app.route_class()
 class ChatRoom(WebsocketView, TemplateView):
-
-    async def get_bot_handler(self, ws_group, msg_data):
-        bot = ws_group['bot']
-        if not bot:
-            bot = ws_group['bot'] = ChatBot(ws_group)
-        response = await bot.handle_msg(msg_data)
-        return response
 
     @app.route('room/ws/{group_name}/', name='room_ws')
     async def ws_handler(self, *args, **kwargs):
         request = self._request
-        response, ws_group = await self.get_ws_response()
+        response = await self.get_ws_response()
         session = await get_session(request)
         async for msg in response:
             if msg.type == aiohttp.web.WSMsgType.TEXT:
                 msg_data = msg.data 
-                if msg_data and msg_data[0] == '!':
-                    bot_res = await self.get_bot_handler(ws_group, msg_data)
-                    msg_resp = bot_res['msg']
-                else:
-                    identity = await get_identity(session)
-                    username = 'User ' + identity['_session_id'][:5]
-                    msg_resp = '{}: {}'.format(username, msg.data)
-                for ws in ws_group['sockets']:
+                identity = await get_identity(session)
+                username = 'User ' + identity['_session_id'][:5]
+                msg_resp = '{}: {}'.format(username, msg.data)
+                for ws in self.ws_group['sockets']:
                     await ws.send_str(msg_resp)
         return response
 
@@ -103,4 +48,43 @@ class ChatRoom(WebsocketView, TemplateView):
         }
         return render_template('chat/chat.html', request, context)
 
-app_views = [ChatRoom]
+
+@app.route_class()
+class ChatBotRoom(WebsocketBotView, TemplateView):
+
+    @app.route('botroom/ws/{group_name}/', name='botroom_ws')
+    async def ws_handler(self, *args, **kwargs):
+        request = self._request
+        response = await self.get_ws_response()
+        session = await get_session(request)
+        async for msg in response:
+            if msg.type == aiohttp.web.WSMsgType.TEXT:
+                msg_data = msg.data 
+                if msg_data and msg_data[0] == '!':
+                    await self.ws_group['bot'].handle_msg(msg_data)
+                else:
+                    identity = await get_identity(session)
+                    username = 'User ' + identity['_session_id'][:5]
+                    msg_resp = '{}: {}'.format(username, msg.data)
+                    for ws in self.ws_group['sockets']:
+                        await ws.send_str(msg_resp)
+        return response
+
+    @app.route('botroom/{group_name}/', name='botroom')
+    async def botroom(self, *args, **kwargs):
+        request = self._request
+        group_name = request._match_info['group_name']
+        ws_url = get_url(request, 'botroom_ws', kwargs={'group_name': group_name}, is_ws=True)
+        session = await get_session(request)
+        # user = await get_user_by_session(session)
+        identity = await get_identity(session)
+        username = 'User ' + identity['_session_id'][:5]
+        context = {
+            'ws_url': ws_url,
+            'title': 'Chatroom {}'.format(group_name),
+            'user': '<{}>'.format(username)
+        }
+        return render_template('chat/chat.html', request, context)
+
+
+app_views = [ChatRoom, ChatBotRoom]
